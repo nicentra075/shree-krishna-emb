2d7326fd736bc4ee2f8a7bd213cb58538a2c6193
Nimesh <nimesh@instancy.com>
2026-05-26 11:54:10 +0530

    feat: add user management main screen with responsive routing

---
+++ shree_krishna_emb_admin/lib/screens/user_management/user_management_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shree_krishna_emb_admin/bloc/user_management/user_list_bloc.dart';
+import 'desktop_user_list_view.dart';
+import 'mobile_user_list_view.dart';
+
+class UserManagementScreen extends StatelessWidget {
+  const UserManagementScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final isMobile = screenSize.width < 768;
+
+    return BlocProvider(
+      create: (context) => UserListBloc(
+        repository: context.read(),
+      ),
+      child: isMobile ? const MobileUserListView() : const DesktopUserListView(),
+    );
+  }
+}
