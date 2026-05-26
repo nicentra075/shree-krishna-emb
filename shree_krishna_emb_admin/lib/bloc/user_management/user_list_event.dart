b82e3a1068a1b2dd0c0e4e2a3e6d5a707d8a64d5
Nimesh <nimesh@instancy.com>
2026-05-26 11:48:47 +0530

    feat: add user list BLoC events

---
+++ shree_krishna_emb_admin/lib/bloc/user_management/user_list_event.dart
@@ -0,0 +1,71 @@
+import 'package:equatable/equatable.dart';
+import 'package:shree_krishna_emb_admin/data/models/user_list_item_model.dart';
+
+abstract class UserListEvent extends Equatable {
+  const UserListEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class LoadUsersEvent extends UserListEvent {
+  const LoadUsersEvent();
+}
+
+class SearchUsersEvent extends UserListEvent {
+  final String query;
+
+  const SearchUsersEvent(this.query);
+
+  @override
+  List<Object?> get props => [query];
+}
+
+class NextPageEvent extends UserListEvent {
+  const NextPageEvent();
+}
+
+class PreviousPageEvent extends UserListEvent {
+  const PreviousPageEvent();
+}
+
+class GoToPageEvent extends UserListEvent {
+  final int pageNumber;
+
+  const GoToPageEvent(this.pageNumber);
+
+  @override
+  List<Object?> get props => [pageNumber];
+}
+
+class EditUserEvent extends UserListEvent {
+  final UserListItemModel user;
+
+  const EditUserEvent(this.user);
+
+  @override
+  List<Object?> get props => [user];
+}
+
+class SuspendUserEvent extends UserListEvent {
+  final String userId;
+  final bool suspend;
+
+  const SuspendUserEvent(this.userId, this.suspend);
+
+  @override
+  List<Object?> get props => [userId, suspend];
+}
+
+class DeleteUserEvent extends UserListEvent {
+  final String userId;
+
+  const DeleteUserEvent(this.userId);
+
+  @override
+  List<Object?> get props => [userId];
+}
+
+class RefreshUsersEvent extends UserListEvent {
+  const RefreshUsersEvent();
+}
